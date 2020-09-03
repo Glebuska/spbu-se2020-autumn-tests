0f1e4dfaffed8750700a59609e0010f46b936cb3
Gleb <gleb.mirzazyanov@gmail.com>
2020-09-03 13:54:41 +0300

    stdout/stderr tests added

--- cmocka_test/main_test.c
+++ cmocka_test/main_test.c
@@ -1,24 +1,13 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
-#include "cmocka.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef _WIN32
-/* Compatibility with the Windows standard C library. */
-#define vsnprintf _vsnprintf
-#endif /* _WIN32 */
-
-#define array_length(x) (sizeof(x) / sizeof((x)[0]))
-
-extern int example_main(int argc, char *argv[]);
-
-int example_test_fprintf(FILE* const file, const char *format, ...) CMOCKA_PRINTF_ATTRIBUTE(2, 3);
-int example_test_printf(const char *format, ...) CMOCKA_PRINTF_ATTRIBUTE(1, 2);
+#include "config.h"
+#include "string.h"
 
 static char temporary_buffer[256];
+static char temporary_buffer_stdout[256];
+static char temporary_buffer_stderr[256];
 static FILE *my_file;
+static int num_of_test;
+static int test1_input[] = {2, 4, 1, 5, 8, 4};
+static int idx;
 
 /* A mock fprintf function that checks the value of strings printed to the
  * standard error stream or output stream. */
@@ -29,7 +18,16 @@ int example_test_fprintf(FILE* const file, const char *format, ...) {
     va_start(args, format);
     return_value = vsnprintf(temporary_buffer, sizeof(temporary_buffer),
                              format, args);
-    check_expected_ptr(temporary_buffer);
+
+    if (file == stdout) {
+        strcpy(temporary_buffer_stdout, temporary_buffer);
+        check_expected_ptr(temporary_buffer_stdout);
+    }
+    else {
+        strcpy(temporary_buffer_stderr, temporary_buffer);
+        check_expected_ptr(temporary_buffer_stderr);
+    }
+
     va_end(args);
     return return_value;
 }
@@ -47,21 +45,30 @@ int example_test_printf(const char *format, ...) {
     return return_value;
 }
 
-int example_test_scanf(const char *format, ...) {
-    int return_value;
+void example_test_scanf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-
-    if (my_file == NULL) {
-        if ((my_file = fopen("test_1.txt", "r")) == NULL) {
-            printf("Cannot open file.\n");
-            exit(1);
-        }
-    }
-    return_value = vfscanf(my_file, format, args);
+    *va_arg(args, int*) = test1_input[idx++];
+    if (array_length(test1_input) == idx) *va_arg(args, char*) = '\n';
     va_end(args);
-    return return_value;
 }
+
+//int example_test_scanf(const char *format, ...) {
+//    int return_value;
+//
+//    va_list args;
+//    va_start(args, format);
+//
+//    if (my_file == NULL) {
+//        if ((my_file = fopen("test_1.txt", "r")) == NULL) {
+//            printf("Cannot open file.\n");
+//            exit(1);
+//        }
+//    }
+//    return_value = vfscanf(my_file, format, args);
+//    va_end(args);
+//    return return_value;
+//}
 
 static void test_example_main_no_args(void **state) {
     const char *args[] = {
@@ -74,15 +81,32 @@ static void test_example_main_no_args(void **state) {
 }
 
 static void test_example_main(void **state) {
+    num_of_test = 1;
+    idx = 0;
     const char *args[] = {
             "example", "--from=3",
     };
 
     (void) state; /* unused */
 
-    expect_string(example_test_fprintf, temporary_buffer, "2 ");
-    expect_string(example_test_fprintf, temporary_buffer, "1 ");
+    expect_string(example_test_fprintf, temporary_buffer_stdout, "2 ");
+    expect_string(example_test_fprintf, temporary_buffer_stdout, "1 ");
     expect_string(example_test_printf, temporary_buffer, "3");
+
+    assert_int_equal(example_main(array_length(args), (char **) args), 0);
+}
+
+static void test_1_main(void **state){
+    num_of_test = 2;
+    const char *args[] = {
+            "example", "--to=9", "--from=3"
+    };
+
+    (void) state; /* unused */
+
+    expect_string(example_test_fprintf, temporary_buffer_stdout, "1");
+    expect_string(example_test_fprintf, temporary_buffer_stderr, "10");
+    expect_string(example_test_fprintf, temporary_buffer_stdout, "3");
 
     assert_int_equal(example_main(array_length(args), (char **) args), 0);
 }
@@ -94,6 +118,6 @@ int main()
             cmocka_unit_test(test_example_main),
     };
 
-    if (my_file != NULL) fclose(my_file);
+    //if (my_file != NULL) fclose(my_file);
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
